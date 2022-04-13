df707b335621ea35433e10cfd4983f31a3d64f6c
rahulj.inexture <rahuj.inexture@gmail.com>
2022-04-13 16:18:40 +0530

    New: created user get api method route.

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,9 +3,18 @@ var userRouter = express.Router();
 const Users = require("../models/userSchema");
 
 
-/* GET users listing. */
-userRouter.get('/users', function(req, res) {
-  res.send('respond with a resource');
+// GET users listing.
+userRouter.get('/users', async (req, res) => {
+  try {
+    const usersData = await Users.find();
+    console.log(usersData);
+    usersData.length > 0 ?
+      res.status(200).send({ status_code: 200, message: "Fetched all users data.", data: usersData }) :
+      res.status(200).send({ status_code: 200, message: "No users found!!" });
+  } catch (error) {
+    console.log("Error", error.message);
+    res.status(400).send({ error: error.message });
+  }
 });
 
 module.exports = userRouter;
