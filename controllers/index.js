00391d9a4a67166567f54c43651475ce380e24c8
Rahul Jagetia <rahulj.inexture@gmail.com>
2022-04-19 15:05:40 +0530

    Readme updated and removed extra code.

--- controllers/index.js
+++
@@ -1,3 +0,0 @@
-// export * as userController from "./userController";
-const userController = require('./userController');
-module.exports = { userController };
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var routes = require("express").Router();
-// import * as userRouter from "./userRoute.js";
-const userRouter = require('./userRoute');
-
-routes.use("/user", userRouter);
-
-// export default routes;
-module.exports = routes;
